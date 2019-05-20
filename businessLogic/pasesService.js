9aabc77371c67a631214f94137858b86c68ee653
Henry Emanuelle Solis Chacon <hsolisc@cri.CORP.REDBAC.com>
2019-05-20 13:48:36 -0600

    try to controll multi request

--- businessLogic/pasesService.js
+++ businessLogic/pasesService.js
@@ -3,47 +3,6 @@ fs = require('fs'),
 http =  require('https'),
 request = require('request'),
 repository = require('../dataAccess/repository.js');
-const envKey = process.env.TOKEN10;
-
-exports.nuevoPase = function(doc, callback) {
-	var params = {
-		query: doc,
-		collection: 'pases'
-	};
-	
-	if(doc.text = "addPase"){
-
-		var options = { method: 'POST',
-		url: 'https://slack.com/api/chat.postMessage',
-		form: 
-			{ channel: doc.event.channel,
-			  text:'What is the Name?'
-			},
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization' : `Bearer ${envKey}`
-		}
-
-		};
- 
-
-		
-		request(options, function (error, response, body) {
-		  if (error) throw new Error(error);
-
-		  console.log(body);
-
-		});
-
-		
-		
-
-
-
-
-
-	}
-};
 
 exports.challenge = function(doc, callback) {
 	var res = {
--- controllers/pasesController.js
+++ controllers/pasesController.js
@@ -1,31 +1,61 @@
-var pasesService = require('../businessLogic/pasesService.js'); 
+var pasesService = require('../businessLogic/pasesService.js'),
+path = require('path'),
+fs = require('fs'),
+http =  require('https'),
+request = require('request');
+
+const envKey = process.env.TOKEN10;
 
 exports.general = function(eRequest, eResponse) {
-  
 
-  if(eRequest.body.ok){
-    console.log(eRequest.body);
-    eResponse.sendStatus(200);
-  }
+
+ 
   if(eRequest.body.type === 'url_verification'){
     console.log(eRequest.body.type);
 
     pasesService.challenge(eRequest.body, function(data){
-        eResponse.send(data);  
+      eResponse.send(data);  
     });
   }
 
   else if(eRequest.body.event.type === 'message'){
-    
+
     pasesService.nuevoPase(eRequest.body, function(data){
-          
-          eResponse.sendStatus(200);
-      });
+
+      eResponse.sendStatus(200);
+    });
   }
+
   else if(eRequest.body.event.type === 'app_mention'){
-       pasesService.nuevoPase(eRequest.body)
-       eResponse.sendStatus(200);
+
+   if(eRequest.body.text = "addPase"){
+
+    var options = { method: 'POST',
+    url: 'https://slack.com/api/chat.postMessage',
+    form: 
+    { channel: eRequest.body.event.channel,
+      text:'What is the Name?'
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization' : `Bearer ${envKey}`
+    }
+
   };
+
+  request(options, function (err, res, body) {
+    if (err) throw new Error(err);
+
+    console.log(body);
+    eResponse.status(200).json({
+          statusCode: 200,
+          status: 'OK'
+    });
+
+  });
+
+  }
+};
 
 };
 
@@ -35,27 +65,27 @@ exports.general = function(eRequest, eResponse) {
 
 exports.getPases = function(eRequest, eResponse) {
   pasesService.getPases(function(data){
-        eResponse.send(data);
-    });
+    eResponse.send(data);
+  });
 };
 
 exports.getPasePorId = function(eRequest, eResponse) {
   pasesService.getPasePorId(eRequest.params.idPase, function(data){
-        eResponse.send(data);
-    });
+    eResponse.send(data);
+  });
 };
 
 
 
 exports.editarPase= function(eRequest, eResponse) {
   pasesService.editarPase(eRequest.params.idPase, eRequest.body, function(data){
-        eResponse.send(data);
-    });
+    eResponse.send(data);
+  });
 };
 
 exports.eliminarPase = function(eRequest, eResponse) {
   pasesService.eliminarPase(eRequest.params.idPase, function(data){
-        eResponse.send(data);
-    });
+    eResponse.send(data);
+  });
 };
 
