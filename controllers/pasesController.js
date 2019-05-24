3f13360fe95af5e4532a1859804107aa2ad17d45
Henry Emanuelle Solis Chacon <hsolisc@cri.CORP.REDBAC.com>
2019-05-24 07:51:52 -0600

    log

--- controllers/pasesController.js
+++ controllers/pasesController.js
@@ -8,7 +8,7 @@ const TOKEN10 = process.env.TOKEN10;
 
 exports.general = function(eRequest, eResponse) {
 
-  console.log(eRequest);
+  console.log(eRequest.body);
 
   if(eRequest.body.type === 'url_verification'){
     pasesService.challenge(eRequest.body, function(data){
