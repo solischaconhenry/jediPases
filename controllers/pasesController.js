12d3ccc631f8608d2004f753e2f80ea69b060deb
Henry Emanuelle Solis Chacon <hsolisc@cri.CORP.REDBAC.com>
2019-08-01 14:38:22 -0600

    correccion errores

--- controllers/pasesController.js
+++ controllers/pasesController.js
@@ -17,7 +17,8 @@ exports.general = function(eRequest, eResponse) {
     });
   }
   console.log(eRequest.body);
-
+  console.log(eRequest.body.event.type);
+  
   //en caso de app_mnention o slash command
   if(eRequest.body.event.type != undefined && eRequest.body.event.type === 'app_mention'){
     //extrae con un split "text": "<@channel> getWSV" el mensaje adjunto con el mention
